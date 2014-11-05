2ac953d0df5dfa5e4ccde27da9dadd9d1d82c8e7
Harris Hancock <vortrab@gmail.com>
2014-11-05 13:43:48 -0800

    Add daemon interface

---
+++ gen-daemon.pb.hpp
@@ -0,0 +1,17 @@
+#ifndef barobo_Daemon_INTERFACE
+#define barobo_Daemon_INTERFACE
+
+#include "rpc/def.hpp"
+#include "daemon.pb.h"
+
+RPCDEF_HPP(
+        // the interface we're defining and it's version triplet
+        (barobo, Daemon), (0, 0, 0),
+        // all methods
+        (getRobotTcpAddress)
+        ,
+        // all broadcasts
+        (robotPluggedIn)
+        )
+
+#endif
